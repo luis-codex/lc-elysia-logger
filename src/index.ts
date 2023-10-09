aa38bbf68abfbacabf6a733bbe8af1647a5e3998
luis-tenorio-code <luisprogaming0@email.com>
2023-10-09 10:21:56 -0500

    updare package.json

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ export const lcLogger = () => {
     .onAfterHandle(({ request, store }) => {
       const logStr = [];
       const forwardedFor = request.headers.get('X-Forwarded-For');
-      const method = methodString({ method: request.method, check: ' ✔ ' });
+      const method = formatRequestMethod(request.method, ' ✔ ');
       const urlPathname = new URL(request.url).pathname;
       const beforeTime = (store as any).beforeTime;
 
@@ -20,14 +20,12 @@ export const lcLogger = () => {
         logStr.push(`[${pc.cyan(forwardedFor)}]`);
       }
 
-      logStr.push(method, urlPathname, durationString(beforeTime));
+      logStr.push(method, urlPathname, formatDuration(beforeTime));
       console.log(logStr.join(' '));
     })
     .onError(({ request, error, store }) => {
       const logStr = [];
-      const method = pc.red(
-        methodString({ method: request.method, check: ' ✗ ' })
-      );
+      const method = pc.red(formatRequestMethod(request.method, ' ✗ '));
       const urlPathname = new URL(request.url).pathname;
       const errorMessage = error.message;
       const beforeTime = (store as any).beforeTime;
@@ -38,67 +36,52 @@ export const lcLogger = () => {
         logStr.push(String(error.status));
       }
 
-      logStr.push(errorMessage, durationString(beforeTime));
+      logStr.push(errorMessage, formatDuration(beforeTime));
       console.log(logStr.join(' '));
     });
 };
-
-function durationString(beforeTime: [number, number]): string {
+function formatDuration(beforeTime: [number, number]): string {
   const [seconds, nanoseconds] = process.hrtime(beforeTime);
-  const durationInMicroseconds = (seconds * 1e9 + nanoseconds) / 1e3; // Convert to microseconds
-  const durationInMilliseconds = (seconds * 1e9 + nanoseconds) / 1e6; // Convert to milliseconds
-  let timeMessage: string = '';
+  const durationInMicroseconds = (seconds * 1e9 + nanoseconds) / 1e3;
+  const durationInMilliseconds = (seconds * 1e9 + nanoseconds) / 1e6;
+
   if (seconds > 0) {
-    timeMessage = `${pc.white('❘')} ${seconds.toPrecision(2)}s`;
+    return `${pc.white('❘')} ${seconds.toPrecision(2)}s`;
   } else if (durationInMilliseconds > 1) {
-    timeMessage = `${pc.white('❘')} ${durationInMilliseconds.toPrecision(2)}ms`;
+    return `${pc.white('❘')} ${durationInMilliseconds.toPrecision(2)}ms`;
   } else if (durationInMicroseconds > 1) {
-    timeMessage = `${pc.white('❘')} ${durationInMicroseconds.toPrecision(4)}µs`;
+    return `${pc.white('❘')} ${durationInMicroseconds.toPrecision(4)}µs`;
   } else if (nanoseconds > 0) {
-    timeMessage = `${pc.white('❘')} ${nanoseconds.toPrecision(4)}ns`;
+    return `${pc.white('❘')} ${nanoseconds.toPrecision(4)}ns`;
   }
 
-  return timeMessage;
+  return '';
 }
 
-function methodString({
-  method,
-  check,
-}: {
-  method: string;
-  check: string;
-}): string {
+function formatRequestMethod(method: string, check: string): string {
   switch (method) {
     case 'GET':
-      // Handle GET request
-      return pc.white(pc.bold(check + 'GET'));
+      return pc.green(pc.bold(check + 'GET'));
 
     case 'POST':
-      // Handle POST request
-      return pc.yellow(pc.bold(check + 'POST'));
+      return pc.blue(pc.bold(check + 'POST'));
 
     case 'PUT':
-      // Handle PUT request
-      return pc.blue(pc.bold(check + 'PUT'));
+      return pc.yellow(pc.bold(check + 'PUT'));
 
     case 'DELETE':
-      // Handle DELETE request
       return pc.red(pc.bold(check + 'DELETE'));
 
     case 'PATCH':
-      // Handle PATCH request
-      return pc.green(pc.bold(check + 'PATCH'));
+      return pc.magenta(pc.bold(check + 'PATCH'));
 
     case 'OPTIONS':
-      // Handle OPTIONS request
-      return pc.gray(pc.bold(check + 'OPTIONS'));
+      return pc.cyan(pc.bold(check + 'OPTIONS'));
 
     case 'HEAD':
-      // Handle HEAD request
-      return pc.magenta(pc.bold(check + 'HEAD'));
+      return pc.gray(pc.bold(check + 'HEAD'));
 
     default:
-      // Handle unknown request method
       return method;
   }
 }
